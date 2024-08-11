77e56ee2a55fba309d80009fbc3aca54bceb1ce3
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-08-11 23:41:51 +0530

    major changes thanks to rohit

--- Neural Network/src/NodeTypes/InputNode.tsx
+++ Neural Network/src/NodeTypes/InputNode.tsx
@@ -14,6 +14,7 @@ type InputNode = Node<
 const InputNode = ({ data }: NodeProps<InputNode>) => {
   //states
   const [params, setParams] = useState(data.layers[0]);
+  const [options, setOptions] = useState();
 
   //query logic
   // ------------------------------
@@ -28,21 +29,80 @@ const InputNode = ({ data }: NodeProps<InputNode>) => {
     console.log(event.target.value);
   }, []);
 
+  const onNumberChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(event.target.value);
+    }, []);
+
+  function getOptions(arg, entry) {
+    if (arg === "**kwargs" || arg === "Name") return null;
+
+    let argele = entry.param_desc[arg];
+    let returndiv;
+    if (argele.type === "dropdown") {
+      let options = entry.param_desc[arg].options.map((opt) => (
+        <option key={opt} value={opt} className="text-xs">
+          {opt}
+        </option>
+      ));
+      returndiv = <select>{options}</select>;
+    } else if (argele.type === "float") {
+      returndiv = (
+        <input
+          type="number"
+          min={argele.min}
+          max={argele.max}
+          step={0.01}
+          onChange={onNumberChange}
+          className="text-xs w-16 rounded-sm p-0.5"
+        ></input>
+      );
+    } else if (argele.type === "int")
+      returndiv = (
+        <input
+          type="number"
+          min={argele.min}
+          max={argele.max}
+          step={1}
+          pattern="\d+"
+          onChange={onNumberChange}
+          className="text-xs"
+        ></input>
+      );
+    else return null;
+
+    return (
+      <div key={crypto.randomUUID()} className="text-xs">
+        <label htmlFor={arg} className="text-[8px]">
+          {arg}
+          <br />
+        </label>
+        {returndiv}
+      </div>
+    );
+  }
+
   const onLayerTypeChange = useCallback(
     (e: React.ChangeEvent<HTMLSelectElement>) => {
       console.log(e.target.value);
       const entry = data.layers.find((ele) => ele.Type === e.target.value);
       console.log(entry);
       setParams(entry);
+      let args = Object.keys(entry.params.args);
+      let kwargs = Object.keys(entry.params.kwargs);
+      let options = [...args, ...kwargs];
+      let optionDivs = options.map((arg) => getOptions(arg, entry));
+      optionDivs = optionDivs.filter((n) => n);
+      setOptions(optionDivs);
     },
-    [params]
+    []
   );
 
   //useEffect
   //-------------------------------
   // useEffect(() => {
   //   console.log(params); // Logs the updated value
-    
+
   // }, [params]);
 
   //layer items
@@ -63,7 +123,7 @@ const InputNode = ({ data }: NodeProps<InputNode>) => {
     <>
       <div className="bg-slate-200 rounded-md shadow-md px-4 py-3 gap-1 flex flex-col">
         <Handle type="source" position={Position.Bottom} id="a" />
-        <h1 className="text-l">{params.Type}</h1>
+        <h1 className="text-l">Input Node</h1>
         <label htmlFor="types" className="text-[8px]">
           Layer Type
         </label>
@@ -83,10 +143,8 @@ const InputNode = ({ data }: NodeProps<InputNode>) => {
         >
           {layerItems}
         </select>
-
-        <div className="flex flex-wrap">
-          {}
-        </div>
+        {/* Set Arguments for Layers */}
+        <div className="flex flex-wrap justify-between">{options}</div>
       </div>
     </>
   );
